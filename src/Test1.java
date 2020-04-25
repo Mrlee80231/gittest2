b8b31fdf425299774baff91f2054004f0120e39f
Mr_lee <ltt30660539@qq.com>
2020-04-25 21:03:43 +0800

    test2提交

---
+++ src/Test1.java
@@ -0,0 +1,17 @@
+/*
+ *@Author: Micro-lee
+ *@Description:TODO
+ *@Date: 2020/4/25 20:27
+ *@Version: V1.0
+ *Modified by
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.out.println ("我是Test2");
+
+    }
+
+}
+
+ 
---
+++ src/Test3.java
@@ -0,0 +1,15 @@
+/*
+ *@Author: Micro-lee
+ *@Description:TODO
+ *@Date: 2020/4/25 20:29
+ *@Version: V1.0
+ *Modified by
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println ("我是Test2");
+    }
+
+}
+
+ 
